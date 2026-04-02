d83d74ef089101e47c5c4f24d79ab6f2afc74723
Tanu1490 <tanushree110803@gmail.com>
2026-04-02 11:15:16 +0530

    initial commit

--- 2. Sorting/2_Bubble.cpp
+++ 2. Sorting/2_Bubble.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
-{
+void bubbleSort(int arr[], int n){
+
+  // BRUTE FORCE - O(n^2) -> worst & avg case
+  // for(int i = n-1 ; i >= 0 ; i--){
+  //   for(int j = 0 ; j <= i-1 ; j++){
+  //     if(arr[j] > arr[j+1]){
+  //       // swap
+  //       int temp = arr[j+1];
+  //       arr[j+1] = arr[j];
+  //       arr[j] = temp;
+  //     }
+  // }
+  // }
+
+  // OPTIMIZED - O(n) -> best case
+  for(int i = n-1 ; i >= 0 ; i--){
+    int didSwap = 0;
+    for(int j = 0 ; j <= i-1 ; j++){
+      if(arr[j] > arr[j+1]){
+        // swap
+        int temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+        didSwap = 1;
+        }
+      }
+      if(didSwap == 0){
+          break;
+      }
+  }
   
 }
 
@@ -18,5 +46,11 @@ int main()
     cin >> arr[i];
   }
   bubbleSort(arr, n);
+  
+   cout <<"Sorted array : ";
+  for (int i = 0; i < n; i++)
+  {
+     cout<<arr[i] << " ";
+  }
   return 0;
 }
--- 2. Sorting/3_Insertion.cpp
+++ 2. Sorting/3_Insertion.cpp
@@ -2,6 +2,25 @@
 using namespace std;
  
 int main(){
- 
+  int n = 6;
+  int arr[n] = {13,46,24,52,20,9} ;
+
+  for(int i = n-1 ; i >=0 ; i-- ){
+
+    for(int j = 0 ; j <= i-1 ; j++){
+        if(arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      
+  }
+  }
+
+  for(int i= 0 ; i <= n-1 ; i++){
+    cout<<arr[i]<<" ";
+  }
+  
+  
   return 0;
 }
